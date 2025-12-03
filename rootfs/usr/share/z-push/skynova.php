1ed6e3565054062ab31ed5d585749765b1a6478a
Lucas Fontes <lucas@cosmonic.com>
2025-12-03 10:15:35 -0500

    broken (config.php)
    Signed-off-by: Lucas Fontes <lucas@cosmonic.com>

--- rootfs/usr/share/z-push/skynova.php
+++ rootfs/usr/share/z-push/skynova.php
@@ -8,5 +8,33 @@ define('LOGAUTHFAIL', true);
 define('RETRY_AFTER_DELAY', 30);
 define('BACKEND_PROVIDER', 'BackendIMAP');
 
+// autodiscover/config.php
+define('AUTODISCOVER_LOGIN_TYPE', AUTODISCOVER_LOGIN_EMAIL);
+
+// backend/ipcmemcached/config.php
+define('MEMCACHED_SERVERS','localhost:11211');
+
+// backend/imap/config.php
 define('IMAP_FOLDER_CONFIGURED', true);
+define('IMAP_SERVER', 'imap-ha.skymail.net.br');
+define('IMAP_PORT', 993);
+define('IMAP_OPTIONS', '/ssl/norsh');
+define('IMAP_FOLDER_INBOX', 'INBOX');
+define('IMAP_FOLDER_SENT', 'Itens Enviados');
+define('IMAP_FOLDER_DRAFT', 'Rascunhos');
+define('IMAP_FOLDER_TRASH', 'Itens Excluídos');
+define('IMAP_FOLDER_SPAM', 'Spam');
+define('IMAP_FOLDER_ARCHIVE', 'Archive');
+
+define('IMAP_SMTP_METHOD', 'smtp');
+global $imap_smtp_params;
+// SMTP Parameters
+//      mail : no params
+$imap_smtp_params = array(
+  "host" => "ssl://smtp-ha.skymail.net.br",
+  "port" => 993,
+  "auth" => true,
+  "username" => "imap_username",
+  "password" => "imap_password"
+);
 ?>
